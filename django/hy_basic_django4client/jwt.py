2c3e2f6ab0d1e26c4eff10078d3a1c43bfb9e3f3
Andrea Antonello <andrea.antonello@gmail.com>
2024-08-05 14:58:50 +0200

    add api login

--- django/hy_basic_django4client/jwt.py
+++ django/hy_basic_django4client/jwt.py
@@ -7,6 +7,15 @@ from rest_framework.decorators import (authentication_classes, permission_classe
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK, HTTP_403_FORBIDDEN
+from django.contrib.auth.models import User
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 @authentication_classes([JWTAuthentication]) 
 @permission_classes([IsAuthenticated]) 
@@ -38,6 +47,23 @@ class RESTAuthHandler:
                 return user 
             except Token.DoesNotExist: 
                 return None 
+
+
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide username and password.'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        LOGGER.warning(f"Unable to login with user '{username}'")
+        return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key, 'id': user.id}, status=HTTP_200_OK)
+
 
 class JWTAuthHandler:
     """
@@ -181,3 +207,8 @@ class JWTAuthHandler:
             urlpatterns.append(path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'))
         if 'rest_framework_simplejwt.views.TokenVerifyView' not in urlpatterns:
             urlpatterns.append(path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'))
+        if 'api/login/' not in urlpatterns:
+            urlpatterns.append(path('api/login/', login, name='login'))
+
+
+
