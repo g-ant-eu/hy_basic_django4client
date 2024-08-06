b95b0d2bf86a3e7f0cf17649b798e53a44b45348
Andrea Antonello <andrea.antonello@gmail.com>
2024-08-06 09:06:39 +0200

    some cleanup

--- django/hy_basic_django4client/config.py
+++ django/hy_basic_django4client/config.py
@@ -150,7 +150,7 @@ def configure_django(settings):
         if not os.path.exists(settings.STATIC_ROOT):
             os.makedirs(settings.STATIC_ROOT)
     if not hasattr(settings, 'STATIC_URL') or not settings.STATIC_URL:
-        settings.STATIC_URL = f'/{staticFolderName}/'
+        settings.STATIC_URL = f'/' #{staticFolderName}/'
 
     if not hasattr(settings, 'STORAGES'):
         settings.STORAGES = {
